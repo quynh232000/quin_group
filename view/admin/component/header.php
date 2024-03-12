d681d8dcb884c876d289c82441aaced24fc4db54
quynh232000 <quynh@gmail.com>
2024-03-12 11:58:40 +0700

    Feature: check amin  permission

--- view/admin/component/header.php
+++ view/admin/component/header.php
@@ -149,14 +149,14 @@
         </li>
         <li class="nav-item dropdown d-none d-lg-block user-dropdown">
           <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-            <img class="img-xs rounded-circle" src="view/admin/images/faces/face8.jpg" alt="Profile image"> </a>
+            <img class="img-xs rounded-circle" src="<?='assest/upload/'.Session::get('avatar') ?>" alt="Profile image"> </a>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
             <div class="dropdown-header text-center">
-              <img class="img-md rounded-circle" src="view/admin/images/faces/face8.jpg" alt="Profile image">
-              <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
-              <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+              <img class="img-md rounded-circle" style="width:32px;height:32px" src="<?='assest/upload/'.Session::get('avatar') ?>" alt="Profile image">
+              <p class="mb-1 mt-3 font-weight-semibold"><?=Session::get('full_name') ?></p>
+              <p class="fw-light text-muted mb-0"><?=Session::get('email') ?></p>
             </div>
-            <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
+            <a href="?mod=profile&act=profile" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
             <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
             <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
             <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
