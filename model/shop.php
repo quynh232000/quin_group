61ac9d73138f3841c0c2ae5408e76878c0f68183
quynh232000 <quynh@gmail.com>
2024-03-12 20:50:38 +0700

    Feature:seller/ Complete manage review

--- model/shop.php
+++ model/shop.php
@@ -131,6 +131,20 @@ class Shop
             return new Response(false, 'fail');
         }
     }
+    // get star shop
+    public function get_star_shop($shop_id = '')
+    {
+        if (empty($id)) {
+            $user_id = Session::get('id');
+            $shop_id = $this->db->select("SELECT avg(r.level) FROM shop where user_id = '$user_id'")->fetchColumn();
+        }
+        return $this->db->select("SELECT avg(r.level) 
+        FROM product_review  r
+        INNER JOIN product p
+        on p.id = r.product_id
+        where p.shop_id = '$shop_id'")->fetchColumn();
+    }
+    
 
 
 
